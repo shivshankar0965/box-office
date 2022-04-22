83447d4a8801612b766cf160f0be9b2ed4d8a901
Shiv Shankar <shivshankar0965@gmail.com>
2022-04-22 08:39:10 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>This is home page.</div>} />
+      <Route path="/starred" element={<div>This is starred page.</div>} />
+      <Route path="/*" element={<div>This is 404 page.</div>} />
+    </Routes>
   );
 }
 
